318fd0462cde561cae3d39b05c56adf67451549b
EnderBombz <jgbbpc@hotmail.com>
2021-09-01 19:27:08 -0300

    Update 2.0.0
    Update 2.0.0

--- routes/authentication/auth.js
+++ routes/authentication/auth.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../../middleware/auth')
 
 router.post("/", authController.auth);
 
-//router.use(authMiddleware);
+router.use(authMiddleware);
 
 router.get("/users", authController.users)
 
