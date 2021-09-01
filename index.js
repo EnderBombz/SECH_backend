42c4508640fb4a9388cd324c1aa1e36602312ac7
EnderBombz <jgbbpc@hotmail.com>
2021-09-01 19:32:58 -0300

    hotfix 2.0.1

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.get("/", (req, res) => {
 
 app.use(routers);
 
-app.listen(5000, () => {
-    console.log('running on port 5000')
+app.listen(process.env.PORT || 5000, () => {
+    console.log('running on port 3000')
 })
