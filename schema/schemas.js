460b8d1c8e6ae4e08c4168225a0f5ea7bc03052b
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2021-09-10 09:56:04 -0300

    Update 5.0.1 Has been changed the werehouse schema

--- schema/schemas.js
+++ schema/schemas.js
@@ -1,47 +1,48 @@
 const mongoose = require("./../database/index");
 
 const Estoque = mongoose.model("estoque", {
-    id_equip: Number,
-    equip_type: String,
-    equip_description: String,
-    equip_provider: String,
-    equip_spec: String,
-    equip_state: String,
-})
+  equip_type: String,
+  equip_details: String,
+  equip_spec: String,
+  equip_state: String
+});
 
 const Usuarios = mongoose.model("usuarios", {
-    cpf: Number,
-    userlevel: String,
-    username: String,
-    password: String,
-    email: String,
-    department: String,
-})
+  cpf: Number,
+  userlevel: String,
+  username: String,
+  password: String,
+  email: String,
+  department: String
+});
 
 const Devolucao = mongoose.model("devolucao", {
-    id_devolution: Number,
-    id_equip: String,
-    id_user: String,
-    devolution_date: Date,
-})
+  id_devolution: Number,
+  id_equip: String,
+  id_user: String,
+  devolution_date: Date
+});
 
 const SolicitacaoEquipamento = mongoose.model("solicitacao_equipamento", {
-    id_request: Number,
-    id_equip: String,
-    id_user: String,
-    devolution_date: Date,
-})
+  id_request: Number,
+  id_equip: String,
+  id_user: String,
+  devolution_date: Date
+});
 
 const SolicitacaoManutencao = mongoose.model("solicitacao_manutencao", {
-    id_maintance: Number,
-    id_equip: String,
-    id_user: String,
-    maintance_date: Date,
-    maintace_type: String,
-    maintance_state: String,
-})
+  id_maintance: Number,
+  id_equip: String,
+  id_user: String,
+  maintance_date: Date,
+  maintace_type: String,
+  maintance_state: String
+});
 
-
-
-
-module.exports = { Estoque, Usuarios, Devolucao, SolicitacaoEquipamento, SolicitacaoManutencao };
+module.exports = {
+  Estoque,
+  Usuarios,
+  Devolucao,
+  SolicitacaoEquipamento,
+  SolicitacaoManutencao
+};
