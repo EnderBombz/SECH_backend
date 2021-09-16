19b2ebdfc517079ef69ae163a62be58aedc42f40
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2021-09-16 09:51:20 -0300

    Werehouse Schema update

--- schema/schemas.js
+++ schema/schemas.js
@@ -1,42 +1,47 @@
 const mongoose = require("./../database/index");
 
 const Werehouse = mongoose.model("werehouse", {
-    equip_type: String,
-    equip_description: String,
-    equip_spec: String,
-    equip_state: String,
-})
+  equip_type: String,
+  equip_class: String,
+  equip_name: String,
+  equip_description: String,
+  equip_spec: String,
+  equip_state: String
+});
 
 const User = mongoose.model("user", {
-    cpf: String,
-    userlevel: String,
-    username: String,
-    password: String,
-    email: String,
-    department: String,
-})
+  cpf: String,
+  userlevel: String,
+  username: String,
+  password: String,
+  email: String,
+  department: String
+});
 
 const Devolution = mongoose.model("devolution", {
-    equip_list: Array,
-    user_id: String,
-    devolution_date: String,
-    devolution_details: String,
-})
+  equip_list: Array,
+  user_id: String,
+  devolution_date: String,
+  devolution_details: String
+});
 
 const EquipmentRequest = mongoose.model("equipment_request", {
-    equip_list: Array,
-    user_id: String,
-    request_date: String,
-})
+  equip_list: Array,
+  user_id: String,
+  request_date: String
+});
 
 const MaintanceRequest = mongoose.model("maintance_request", {
-    equip_list: Array,
-    user_id: String,
-    request_date: Date,
-    maintace_details: String,
-})
+  equip_list: Array,
+  user_id: String,
+  request_date: Date,
+  maintace_details: String
+});
 
-
-
-
-module.exports = { Werehouse, User, Devolution, EquipmentRequest, MaintanceRequest };
+module.exports = {
+  Werehouse,
+  User,
+  Devolution,
+  EquipmentRequest,
+  MaintanceRequest
+};
