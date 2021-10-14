0e418529def5534cbf63b0e09a1c0795ef1f1d6b
EnderBombz <61323617+EnderBombz@users.noreply.github.com>
2021-10-14 07:57:17 -0300

    Authmiddlewar descomentado

--- routes/router.js
+++ routes/router.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require("./authentication/auth")
+const auth = require("./authentication/auth");
 
+const devolution = require("./devolution");
+const maintance = require("./maintance");
+const users = require("./users");
+const werehouse = require("./werehouse");
+const equipmentRequest = require("./equipmentRequest");
 
-const devolution = require('./devolution');
-const maintance = require("./maintance")
-const users = require("./users")
-const werehouse = require('./werehouse')
-const equipmentRequest = require("./equipmentRequest")
-
-const authMiddleware = require('../middleware/auth')
-
+const authMiddleware = require("../middleware/auth");
 
 router.use("/api/auth", auth);
-//router.use(authMiddleware);
+router.use(authMiddleware);
 router.use("/devolution", devolution);
 router.use("/maintance", maintance);
 router.use("/users", users);
 router.use("/equipment-requests", equipmentRequest);
 router.use("/werehouse", werehouse);
-
 
 module.exports = router;
